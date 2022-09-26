af8ae6a4e5f710ea91010315d825edfe48341b75
RickyY1689 <richard.jr.yang@gmail.com>
2022-09-26 01:51:52 -0400

    adding more work from activity 4 that was missed - update messages

--- hello.py
+++ hello.py
@@ -1,11 +1,10 @@
 from datetime import datetime
-from xml.dom import ValidationErr
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, EmailField, SubmitField
-from wtforms.validators import DataRequired, StopValidation
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = 'noonewillguessthis'
@@ -20,12 +19,15 @@ class NameAndEmailForm(FlaskForm):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
-    email = None
     form = NameAndEmailForm()
     if form.validate_on_submit():
-        name = form.name.data
-        email = form.email.data
-    form.name.data = ''
-    form.email.data = ''
-    return render_template('index.html', form=form, name=name, email=email)
+        old_name = session.get('name')
+        old_email = session.get('email')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        if old_email is not None and old_name != form.email.data:
+            flash('Looks like you have changed your email!')
+        session['name'] = form.name.data
+        session['email'] = form.email.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
